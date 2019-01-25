86684e5a812c23ab400539cb6866b35d6772d11d
perlancar (on pc-office) <perlancar@gmail.com>
2019-01-25 15:03:09 +0700

    Initial commit

---
+++ lib/App/NumberUtilRangeUtils.pm
@@ -0,0 +1,18 @@
+package App::NumberUtilRangeUtils;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT:
+
+=head1 DESCRIPTION
+
+This distribution includes the following command-line utilities:
+
+# INSERT_EXECS_LIST
+
+
+=head1 SEE ALSO
+
+L<Number::Util::Range>
